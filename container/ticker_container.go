37480f311cf2c246996cfe9e4315d300407d293c
Will Shi <drinkthere@gmail.com>
2024-12-30 10:19:38 +0800

    push ticker only when price/qty changes

--- container/ticker_container.go
+++ container/ticker_container.go
@@ -5,6 +5,13 @@ import (
 	"sync"
 )
 
+type LastTicker struct {
+	BidPrice float64 // 买1价
+	BidSize  float64 // 买1量
+	AskPrice float64 // 卖1价
+	AskSize  float64 // 卖1量
+}
+
 type TickerWrapper struct {
 	Exchange config.Exchange
 	InstType config.InstrumentType
@@ -17,6 +24,7 @@ type TickerWrapper struct {
 	AskSize      float64 // 卖1量
 	UpdateTimeMs int64   //更新时间（微秒）
 	UpdateID     int64   // 更新ID 400900217
+	LastTicker   LastTicker
 }
 
 func (wrapper *TickerWrapper) updateTicker(message TickerWrapper) bool {
@@ -33,14 +41,28 @@ func (wrapper *TickerWrapper) updateTicker(message TickerWrapper) bool {
 		}
 	}
 
+	if message.AskPrice == wrapper.LastTicker.AskPrice &&
+		message.BidPrice == wrapper.LastTicker.BidPrice &&
+		message.AskSize == wrapper.LastTicker.AskSize &&
+		message.BidSize == wrapper.LastTicker.BidSize {
+		// ticker值没有变化，返回false为了不推送重复的消息到做市服务，但是updateTime和updateID 还是会更新
+		wrapper.UpdateTimeMs = message.UpdateTimeMs
+		wrapper.UpdateID = message.UpdateID
+		return false
+	}
+
 	if message.AskPrice > 0.0 && message.AskSize > 0.0 {
 		wrapper.AskPrice = message.AskPrice
 		wrapper.AskSize = message.AskSize
+		wrapper.LastTicker.AskPrice = message.AskPrice
+		wrapper.LastTicker.AskSize = message.AskSize
 	}
 
 	if message.BidPrice > 0.0 && message.BidSize > 0.0 {
 		wrapper.BidPrice = message.BidPrice
 		wrapper.BidSize = message.BidSize
+		wrapper.LastTicker.BidPrice = message.BidPrice
+		wrapper.LastTicker.BidSize = message.BidSize
 	}
 
 	wrapper.UpdateTimeMs = message.UpdateTimeMs
